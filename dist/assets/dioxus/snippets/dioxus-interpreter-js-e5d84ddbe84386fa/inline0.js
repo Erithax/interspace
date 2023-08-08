c2df5980ed8175ae415bd86ca8d814ab5528def8
erithax <erithax@proton.me>
2023-08-08 22:42:52 +0200

    distribution build

--- dist/assets/dioxus/snippets/dioxus-interpreter-js-e5d84ddbe84386fa/inline0.js
+++ dist/assets/dioxus/snippets/dioxus-interpreter-js-e5d84ddbe84386fa/inline0.js
@@ -163,19 +163,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let u8buf,u8bufp;const attr = [];
-                    let attr_tmp1, attr_tmp2;
-                    function get_attr() {
-                        attr_tmp2 = u8buf[u8bufp++];
-                        if(attr_tmp2 & 128){
-                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            attr[attr_tmp2&4294967167]=attr_tmp1;
-                            return attr_tmp1;
-                        }
-                        else{
-                            return attr[attr_tmp2&4294967167];
-                        }
-                    }const evt = [];
+    const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -186,6 +174,18 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         }
                         else{
                             return evt[evt_tmp2&4294967167];
+                        }
+                    }let u32buf,u32bufp;const attr = [];
+                    let attr_tmp1, attr_tmp2;
+                    function get_attr() {
+                        attr_tmp2 = u8buf[u8bufp++];
+                        if(attr_tmp2 & 128){
+                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            attr[attr_tmp2&4294967167]=attr_tmp1;
+                            return attr_tmp1;
+                        }
+                        else{
+                            return attr[attr_tmp2&4294967167];
                         }
                     }const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
@@ -199,8 +199,8 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
-                    }let u32buf,u32bufp;let s = "";let lsp,sp,sl; let c = new TextDecoder();
-            let len,ns,bubbles,event_name,ptr,id,field,value;
+                    }let u8buf,u8bufp;let s = "";let lsp,sp,sl; let c = new TextDecoder();
+            let field,ptr,event_name,bubbles,value,len,id,ns;
             export function create(r){
                 d=r;
             }
@@ -213,13 +213,13 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     ls=m.getUint32(d+12*4,true);
                 }
                 p=ls;
-                if ((metaflags>>>5)&1){
-                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
-            }
-            u8bufp=0;if ((metaflags>>>3)&1){
+                if ((metaflags>>>3)&1){
                 u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
             }
-            u32bufp=0;if (metaflags&1){
+            u32bufp=0;if ((metaflags>>>5)&1){
+                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
+            }
+            u8bufp=0;if (metaflags&1){
                 lsp = m.getUint32(d+1*4,true);
             }
             if ((metaflags>>>2)&1) {
